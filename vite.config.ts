2daeb50e0c50a5df551483187a58edb489d98b54
Secure Wipe User <securewipe@example.com>
2025-10-05 10:09:37 +0530

    rename dist to docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/securewipeweb/",
   server: {
     host: "::",
     port: 8080,
